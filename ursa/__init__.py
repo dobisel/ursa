930dd897ea2e605fa3238aa0dbbe8f593ac9b1fe
mehrdad1373pedramfar <mehrdad1373pedramfar@yahoo.com>
2018-01-09 10:07:21 +0330

    v0.1.2-dev.0

--- ursa/__init__.py
+++ ursa/__init__.py
@@ -6,7 +6,7 @@ from ursa.authentication import Authenticator
 from ursa.controllers import Root
 from ursa import basedata
 
-__version__ = '0.1.1-dev.0'
+__version__ = '0.1.2-dev.0'
 
 
 class Ursa(Application):
