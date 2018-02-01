92fb1b48d4bf42fe9841a4b5c45193fcd4fed788
mehrdad1373pedramfar <mehrdad1373pedramfar@yahoo.com>
2018-02-01 18:57:19 +0330

    Update wsgi.py add headers

--- wsgi.py
+++ wsgi.py
@@ -10,6 +10,10 @@ def cross_origin_helper_app(environ, start_response):
 
     def better_start_response(status, headers):
         headers.append(('Access-Control-Allow-Origin', os.environ.get('TRUSTED_HOSTS', '*')))
+        headers.append(('Access-Control-Allow-Credentials', 'true'))
+        headers.append(('Access-Control-Allow-Headers', 'Content-Type, Authorization'))
+        headers.append(('Access-Control-Expose-Headers', 'X-Pagination-Count'))
+        headers.append(('Access-Control-Allow-Methods', 'PUT, REBOOT'))
         start_response(status, headers)
 
     return ursa(environ, better_start_response)
